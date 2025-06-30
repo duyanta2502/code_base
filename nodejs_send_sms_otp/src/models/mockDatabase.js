01daf66dffec879ecb0e3593a14ea6e381b2509f
An <tda25022003@gmail.com>
2025-06-30 09:51:39 +0700

    first commit

---
+++ nodejs_send_sms_otp/src/models/mockDatabase.js
@@ -0,0 +1,6 @@
+export const MOCK_USER = {
+  ID: 'trungquandev-sample-id-12345678',
+  EMAIL: 'trungquandev.official@gmail.com',
+  PASSWORD: 'trungquandev@123',
+  USERNAME: 'TrungQuanDev - Một Lập Trình Viên'
+}
